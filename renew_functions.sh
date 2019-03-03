ebb926da486e12a29390abdd8c79c4ca619e3feb
herc1 <hnstoyanov@gmail.com>
2019-03-03 20:20:46 +0200

    force renew every 2 months

--- renew_functions.sh
+++ renew_functions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 function renew_single() {
-  certbot certonly --standalone -d $1
+  certbot certonly --standalone -n -d $1 --force-renewal
   cat /etc/letsencrypt/live/$1/cert.pem /etc/letsencrypt/live/$1/privkey.pem > /opt/output/$1.pem
 }
