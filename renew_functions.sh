59d86acc11efbd35b097fb1cad0e8a0979ccc96c
herc1 <hnstoyanov@gmail.com>
2019-03-03 19:46:55 +0200

    fix output dir

--- renew_functions.sh
+++ renew_functions.sh
@@ -2,5 +2,5 @@
 
 function renew_single() {
   certbot certonly --standalone -d $1
-  cat /etc/letsencrypt/live/$1/cert.pem /etc/letsencrypt/live/$1/privkey.pem > /etc/ssl/$1.pem
+  cat /etc/letsencrypt/live/$1/cert.pem /etc/letsencrypt/live/$1/privkey.pem > /opt/output/$1.pem
 }
