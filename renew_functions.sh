f06b72ee13d166a1edefeddbe72ed5bc56af4365
Hristo Stoyanov <hstoyanov@Hristos-MacBook-Pro.local>
2019-06-07 16:06:41 +0100

    adding fullchain instead of cert

--- renew_functions.sh
+++ renew_functions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 function renew_single() {
-  certbot certonly --standalone -n -d $1 --force-renewal
-  cat /etc/letsencrypt/live/$1/cert.pem /etc/letsencrypt/live/$1/privkey.pem > /opt/output/$1.pem
+  certbot certonly --standalone -n -d $1 --force-renewal --agree-tos -m "${MAIL}"
+  cat /etc/letsencrypt/live/$1/fullchain.pem /etc/letsencrypt/live/$1/privkey.pem > /opt/output/$1.pem
 }
